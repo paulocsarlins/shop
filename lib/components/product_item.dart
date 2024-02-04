fb89b38c157ffd9ad0f45141b3ceafb1444a1ef0
Paulo César Lins <paulocsar91@gmail.com>
2024-02-04 18:56:40 -0300

    Excluir Produto

--- lib/components/product_item.dart
+++ lib/components/product_item.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:shop/models/product.dart';
+import 'package:shop/models/product_list.dart';
 import 'package:shop/utils/app_routes.dart';
 
 class ProductItem extends StatelessWidget {
@@ -22,15 +24,20 @@ class ProductItem extends StatelessWidget {
               color: Theme.of(context).primaryColor,
               onPressed: () {
                 Navigator.of(context).pushNamed(
-                AppRoutes.product_form,
-                arguments: product,
+                  AppRoutes.product_form,
+                  arguments: product,
                 );
               },
             ),
             IconButton(
               icon: Icon(Icons.delete),
               color: Theme.of(context).errorColor,
-              onPressed: () {},
+              onPressed: () {
+                Provider.of<ProductList>(
+                  context,
+                  listen: false,
+                ).removeProduct(product);
+              },
             ),
           ],
         ),
