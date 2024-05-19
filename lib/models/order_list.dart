bf8709451b56c6afa3903ef86fe58aacb425b5bd
Paulo César Lins <paulocsar91@gmail.com>
2024-05-19 18:31:09 -0300

    Carregando os Pedidos

--- lib/models/order_list.dart
+++ lib/models/order_list.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 import 'package:flutter/cupertino.dart';
 import 'package:http/http.dart' as http;
 import 'package:shop/models/cart.dart';
+import 'package:shop/models/cart_item.dart';
 import 'package:shop/models/order.dart';
 import 'package:shop/utils/constants.dart';
 
@@ -16,6 +17,34 @@ class OrderList with ChangeNotifier {
     return _items.length;
   }
 
+  Future<void> loadOrders() async {
+    _items.clear();
+    final response = await http.get(
+      Uri.parse('${Constants.ORDER_BASE_URL}.json'),
+    );
+    if (response.body == 'null') return;
+    Map<String, dynamic> data = jsonDecode(response.body);
+     data.forEach((orderId, orderData) {
+       _items.add(
+         Order(
+          id: orderId,
+          date: DateTime.parse(orderData['date']),
+          total: orderData['total'],
+          products: (orderData['products'] as List<dynamic>).map((item){
+            return CartItem(
+              id: item['id'],
+              productId: item['productId'],
+              name: item['name'],
+              quantity: item['quantity'],
+              price: item['price'],
+            );
+          }).toList(),
+         ),
+       );
+     });
+     notifyListeners();
+  }
+
   Future<void> addOrder(Cart cart) async {
     final date = DateTime.now();
     final response = await http.post(
@@ -24,13 +53,15 @@ class OrderList with ChangeNotifier {
         {
           'total': cart.totalAmount,
           'date': date.toIso8601String(),
-          'products': cart.items.values.map((cartItem) => {
-                'id': cartItem.id,
-                'productId': cartItem.productId,
-                'name': cartItem.name,
-                'quantity': cartItem.quantity,
-                'price': cartItem.price,
-              }).toList(),
+          'products': cart.items.values
+              .map((cartItem) => {
+                    'id': cartItem.id,
+                    'productId': cartItem.productId,
+                    'name': cartItem.name,
+                    'quantity': cartItem.quantity,
+                    'price': cartItem.price,
+                  })
+              .toList(),
         },
       ),
     );
--- lib/pages/orders_page.dart
+++ lib/pages/orders_page.dart
@@ -4,8 +4,26 @@ import 'package:shop/components/app_drawer.dart';
 import 'package:shop/components/order.dart';
 import 'package:shop/models/order_list.dart';
 
-class OrdersPage extends StatelessWidget {
+class OrdersPage extends StatefulWidget {
   const OrdersPage({super.key});
+
+  @override
+  State<OrdersPage> createState() => _OrdersPageState();
+}
+
+class _OrdersPageState extends State<OrdersPage> {
+  bool _isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    Provider.of<OrderList>(
+      context,
+      listen: false,
+    ).loadOrders().then((_) {
+      setState(() => _isLoading = false);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,10 +33,14 @@ class OrdersPage extends StatelessWidget {
         title: Text('Meus Pedidos'),
       ),
       drawer: AppDrawer(),
-      body: ListView.builder(
-        itemCount: orders.itemsCount,
-        itemBuilder: (ctx, i) => OrderWidget(order: orders.items[i]),
-        ),
+      body: _isLoading
+          ? Center(
+              child: CircularProgressIndicator(),
+            )
+          : ListView.builder(
+              itemCount: orders.itemsCount,
+              itemBuilder: (ctx, i) => OrderWidget(order: orders.items[i]),
+            ),
     );
   }
 }
