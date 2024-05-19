035c21594af4cdae0934b5b6aad199fb52b9167c
Paulo César Lins <paulocsar91@gmail.com>
2024-05-19 18:49:12 -0300

    FutureBuilder

--- lib/pages/orders_page.dart
+++ lib/pages/orders_page.dart
@@ -4,43 +4,36 @@ import 'package:shop/components/app_drawer.dart';
 import 'package:shop/components/order.dart';
 import 'package:shop/models/order_list.dart';
 
-class OrdersPage extends StatefulWidget {
-  const OrdersPage({super.key});
-
-  @override
-  State<OrdersPage> createState() => _OrdersPageState();
-}
-
-class _OrdersPageState extends State<OrdersPage> {
-  bool _isLoading = true;
-
-  @override
-  void initState() {
-    super.initState();
-    Provider.of<OrderList>(
-      context,
-      listen: false,
-    ).loadOrders().then((_) {
-      setState(() => _isLoading = false);
-    });
-  }
-
+class OrdersPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final OrderList orders = Provider.of(context);
     return Scaffold(
       appBar: AppBar(
         title: Text('Meus Pedidos'),
       ),
       drawer: AppDrawer(),
-      body: _isLoading
-          ? Center(
-              child: CircularProgressIndicator(),
-            )
-          : ListView.builder(
-              itemCount: orders.itemsCount,
-              itemBuilder: (ctx, i) => OrderWidget(order: orders.items[i]),
-            ),
+      body: FutureBuilder(
+        future: Provider.of<OrderList>(
+          context,
+          listen: false,
+        ).loadOrders(),
+        builder: (ctx, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else if (snapshot.error != null) {
+            return Center(
+              child: Text('Ocorreu um erro!'),
+            );
+          } else {
+            return Consumer<OrderList>(
+              builder: (ctx, orders, child) => ListView.builder(
+                itemCount: orders.itemsCount,
+                itemBuilder: (ctx, i) => OrderWidget(order: orders.items[i]),
+              ),
+            );
+          }
+        },
+      ),
     );
   }
 }
