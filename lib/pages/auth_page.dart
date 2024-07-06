54383d7c3aedb465eecec738a749f96510474e2c
Paulo César Lins <paulocsar91@gmail.com>
2024-07-06 17:33:03 -0300

    Tela de Autenticação #03

--- lib/pages/auth_page.dart
+++ lib/pages/auth_page.dart
@@ -3,6 +3,7 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:shop/components/auth_form.dart';
 
 class AuthPage extends StatelessWidget {
   const AuthPage({super.key});
@@ -37,23 +38,25 @@ class AuthPage extends StatelessWidget {
                   ),
                   transform: Matrix4.rotationZ(-8 * pi / 180)..translate(-10.0),
                   decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20),
-                    color: Colors.deepOrange.shade900,
-                    boxShadow: [
-                      BoxShadow(
-                        blurRadius: 8,
-                        color: Colors.black26,
-                        offset: Offset(0,2),
-                      )
-                    ]
+                      borderRadius: BorderRadius.circular(20),
+                      color: Colors.deepOrange.shade900,
+                      boxShadow: [
+                        BoxShadow(
+                          blurRadius: 8,
+                          color: Colors.black26,
+                          offset: Offset(0, 2),
+                        )
+                      ]),
+                  child: Text(
+                    'Minha Loja',
+                    style: TextStyle(
+                      fontSize: 45,
+                      fontFamily: 'Anton',
+                      color: Theme.of(context).accentTextTheme.headline6?.color,
+                    ),
                   ),
-                  child: Text('Minha Loja',
-                  style: TextStyle(
-                    fontSize: 45,
-                    fontFamily: 'Anton',
-                    color: Theme.of(context).accentTextTheme.headline6?.color,
-                  ),),
-                )
+                ),
+                AuthForm(),
               ],
             ),
           )
