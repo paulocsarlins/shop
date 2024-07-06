da5a48b663fedb84ab3d37ab6450bc21f1953fce
Paulo César Lins <paulocsar91@gmail.com>
2024-07-06 15:31:33 -0300

    Tela de Autenticação #01

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:provider/provider.dart';
 import 'package:shop/models/cart.dart';
 import 'package:shop/models/order_list.dart';
 import 'package:shop/models/product_list.dart';
+import 'package:shop/pages/auth_page.dart';
 import 'package:shop/pages/cart_page.dart';
 import 'package:shop/pages/orders_page.dart';
 import 'package:shop/pages/product_detail_page.dart';
@@ -43,10 +44,11 @@ class MyApp extends StatelessWidget {
         ),
         // home: const ProductsOverviewPage(),
         routes: {
+          AppRoutes.auth: (ctx) => AuthPage(),
           AppRoutes.home:(ctx) => const ProductsOverviewPage(),
           AppRoutes.productDetail: (ctx) => const ProductDetailPage(),
           AppRoutes.cart: (ctx) => const CartPage(),
-          AppRoutes.orders: (ctx) => const OrdersPage(),
+          AppRoutes.orders: (ctx) => OrdersPage(),
           AppRoutes.products: (ctx) => const ProductsPage(),
           AppRoutes.product_form: (ctx) => const ProductFormPage(),
         },
--- lib/utils/app_routes.dart
+++ lib/utils/app_routes.dart
@@ -1,9 +1,9 @@
 class AppRoutes {
-  static const home = '/';
+  static const auth = '/';
+  static const home = '/home';
   static const productDetail = '/product-detail';
   static const cart = '/cart';
   static const orders = '/orders';
   static const products = '/products';
   static const product_form = '/product_form';
 }
-
