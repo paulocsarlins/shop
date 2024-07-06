fd8993ffde676be32815ec76b3e11fc88b5f8485
Paulo César Lins <paulocsar91@gmail.com>
2024-07-06 16:07:00 -0300

    Tela de Autenticação #02

--- lib/pages/auth_page.dart
+++ lib/pages/auth_page.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
@@ -16,15 +18,42 @@ class AuthPage extends StatelessWidget {
                 colors: [
                   Color.fromRGBO(215, 117, 255, 0.5),
                   Color.fromRGBO(215, 118, 117, 0.9),
-                ]
-              )
+                ],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+              ),
             ),
           ),
           Container(
             width: double.infinity,
             child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                
+                Container(
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 10,
+                    horizontal: 70,
+                  ),
+                  transform: Matrix4.rotationZ(-8 * pi / 180)..translate(-10.0),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.deepOrange.shade900,
+                    boxShadow: [
+                      BoxShadow(
+                        blurRadius: 8,
+                        color: Colors.black26,
+                        offset: Offset(0,2),
+                      )
+                    ]
+                  ),
+                  child: Text('Minha Loja',
+                  style: TextStyle(
+                    fontSize: 45,
+                    fontFamily: 'Anton',
+                    color: Theme.of(context).accentTextTheme.headline6?.color,
+                  ),),
+                )
               ],
             ),
           )
