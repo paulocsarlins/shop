e1563b026a014c714adcfadd6399191be8e31c20
Paulo César Lins <paulocsar91@gmail.com>
2024-07-21 22:14:11 -0300

    Login de Usuário

--- lib/models/auth.dart
+++ lib/models/auth.dart
@@ -7,14 +7,26 @@ class Auth with ChangeNotifier {
   static const _url =
       'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBVUUa4huASjOrCw45bpGK0quH791e6NPw';
 
-  Future<void> signup(String email, String password) async {
+  Future<void> _authenticate(
+      String email, String password, String urlFragment) async {
+    final url =
+        'https://identitytoolkit.googleapis.com/v1/accounts:$urlFragment?key=AIzaSyBVUUa4huASjOrCw45bpGK0quH791e6NPw';
+
     final response = await http.post(
-      Uri.parse(_url),
+      Uri.parse(url),
       body: jsonEncode({
         'email': email,
         'password': password,
         'returnSecureToken': true,
       }),
     );
+  }
+
+  Future<void> signup(String email, String password) async {
+    _authenticate(email, password, 'signUp');
+  }
+  
+  Future<void> login(String email, String password) async {
+    _authenticate(email, password, 'signInWithPassword');
   }
 }
