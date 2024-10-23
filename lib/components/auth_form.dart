084a66727f98397e7e6052a505cb7fa6e8b6dea2
Paulo César Lins <paulocsar91@gmail.com>
2024-10-23 13:14:20 -0300

    Usando Widget AnimatedContainer

--- lib/components/auth_form.dart
+++ lib/components/auth_form.dart
@@ -130,15 +130,13 @@ class _AuthFormState extends State<AuthForm>
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(10),
       ),
-      child: AnimatedBuilder(
-        animation: _heigthAnimation!,
-        builder: (ctx, childForm) => Container(
+      child: AnimatedContainer(
+        duration: Duration(milliseconds: 300),
+        curve: Curves.easeIn,
           padding: const EdgeInsets.all(16),
-          // height: _isLogin() ? 310 : 400,
-          height: _heigthAnimation?.value.height ?? (_isLogin() ? 310 : 400),
+           height: _isLogin() ? 310 : 400,
+        // height: _heigthAnimation?.value.height ?? (_isLogin() ? 310 : 400),
           width: deviceSize.width * 0.75,
-          child: childForm, 
-        ),
           child: Form(
             key: _formKey,
             child: Column(
