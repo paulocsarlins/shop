b30b2b391034aab05f29cd78784b59ddbf015928
Paulo César Lins <paulocsar91@gmail.com>
2024-10-26 19:00:40 -0300

    Animando as Transições entre Telas

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ import 'package:shop/pages/product_detail_page.dart';
 import 'package:shop/pages/product_form_page.dart';
 import 'package:shop/pages/products_page.dart';
 import 'package:shop/utils/app_routes.dart';
+import 'package:shop/utils/custom_route.dart';
 
 void main() {
   runApp(const MyApp());
@@ -56,6 +57,12 @@ class MyApp extends StatelessWidget {
             secondary: Colors.deepOrange,
           ),
           fontFamily: 'Lato',
+          pageTransitionsTheme: PageTransitionsTheme(
+            builders: {
+              TargetPlatform.android: CustomPagetransitionsBuilder(),
+              TargetPlatform.iOS: CustomPagetransitionsBuilder(),
+            }
+          )
         ),
         // home: const ProductsOverviewPage(),
         routes: {
